7ceb2db7b94bccdad009c36a7da76b4d8bcf1787
Abdullah Omar <fishilike.fish@gmail.com>
2020-08-25 19:14:59 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [number, setNumber] = useState(0);
+	const [factorial, setFactorial] = useState(1);
+
+	const handleCalculate = () => {
+		let temp = number;
+		if (number === 0) {
+			return setFactorial(1);
+		}
+		if (temp < 0) {
+			temp = temp * -1;
+		}
+		let result = 1;
+		for (var i = 1; i <= temp; i++) {
+			result *= i;
+		}
+		number < 0 ? setFactorial(result * -1) : setFactorial(result);
+	};
+
+	return (
+		<div>
+			<h1>Factorial Calculator</h1>
+			<form onSubmit={(e) => e.preventDefault()}>
+				<input
+					value={number}
+					onChange={(event) => setNumber(event.target.value)}
+					type='number'
+					placeholder='Enter a number...'
+				/>
+				<br />
+				<button onClick={handleCalculate}>Calculate Factorial</button>
+			</form>
+			<h2>Factorial: {factorial}</h2>
+		</div>
+	);
+};
 
 export default App;
